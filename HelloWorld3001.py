2b5a54ab5982c6666766e0693cc11b73b4d16ecf
paige-car2018 <44813845+paige-car2018@users.noreply.github.com>
2018-11-06 10:16:20 -0700

    Add files via upload

---
+++ HelloWorld3001.py
@@ -0,0 +1,11 @@
+print("Hello World")
+var = 2018
+print("Today is Monday the 5th of November" , var)
+
+print (7 + 8)
+print (7-8)
+print (7*8)
+print (58/5)
+print(5**2)
+
+print ("My name is William Hoover"
